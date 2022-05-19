526b14625550685d5480f48401ba04ddf04b4c06
Selvi1410 <vmpmsadhi@gmail.com>
2022-05-19 15:07:14 +0800

    cis

---
+++ cis.tf
@@ -0,0 +1,4 @@
+resource "aws_securityhub_standards_subscription" "cis" {
+  depends_on    = [aws_securityhub_account.example]
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+}
